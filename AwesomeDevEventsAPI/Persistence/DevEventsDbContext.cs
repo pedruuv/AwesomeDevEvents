785be1efa2a5e805cf71932433eab5803e3885e4
Pedro Vitor <impedrovgurgel@outlook.com>
2023-11-09 10:58:05 -0300

    Initial commit

---
+++ AwesomeDevEventsAPI/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeDevEventsAPI.Entities;
+
+namespace AwesomeDevEventsAPI.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AwesomeDevEvents
+ 
